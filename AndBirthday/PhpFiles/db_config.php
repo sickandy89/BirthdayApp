94f4fb60b3690083e32ff00b458978e52db627db
sickandy89 <andy_da_god@web.de>
2014-08-03 19:55:36 +0200

    Delete db_config.php

--- AndBirthday/PhpFiles/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_USER', "sickandy_bday"); // db user
-define('DB_PASSWORD', "matisic2734262"); // db password (mention your db password here)
-define('DB_DATABASE', "sickandy_bday"); // database name
-define('DB_SERVER', "localhost"); // db server
-
-?>
